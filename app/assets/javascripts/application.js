083b2046c203c01781183feaf9cd1691a2dd4223
Neto <jose.lopes.santos.neto@gmail.com>
2018-08-17 10:58:43 -0300

    tirada a gem materialize para deixar a primeira versão funcionando exatamente como o demo do mmenu.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,74 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery.mmenu.all
 //= require_tree .
+//= require_self
+
+
+
+$(function() {
+    $('nav#menu').mmenu({
+        wrappers: ["turbolinks"],
+        extensions	: [ 'theme-dark' ],
+        setSelected	: true,
+        counters	: true,
+        searchfield : {
+            placeholder		: 'Search menu items'
+        },
+        iconbar		: {
+            add 		: true,
+            size		: 40,
+            top 		: [
+                '<a href="#/"><span class="fa fa-home"></span></a>'
+            ],
+            bottom 		: [
+                '<a href="#/"><span class="fa fa-twitter"></span></a>',
+                '<a href="#/"><span class="fa fa-facebook"></span></a>',
+                '<a href="#/"><span class="fa fa-youtube"></span></a>'
+            ]
+        },
+        sidebar		: {
+            collapsed		: {
+                use 			: '(min-width: 450px)',
+                size			: 40,
+                hideNavbar		: false
+            },
+            expanded		: {
+                use 			: '(min-width: 992px)',
+                size			: 35
+            }
+        },
+        navbars		: [
+            {
+                content		: [ 'searchfield' ]
+            }, {
+                type		: 'tabs',
+                content		: [
+                    '<a href="#panel-menu"><i class="fa fa-bars"></i> <span>Menu</span></a>',
+                    '<a href="#panel-account"><i class="fa fa-user"></i> <span>Account</span></a>',
+                    '<a href="#panel-cart"><i class="fa fa-shopping-cart"></i> <span>Cart</span></a>'
+                ]
+            }, {
+                content		: [ 'prev', 'breadcrumbs', 'close' ]
+            }, {
+                position	: 'bottom',
+                content		: [ '<a href="http://mmenu.frebsite.nl/wordpress-plugin" target="_blank">WordPress plugin</a>' ]
+            }
+        ]
+    }, {
+        searchfield : {
+            clear 		: true
+        },
+        navbars		: {
+            breadcrumbs	: {
+                removeFirst	: true
+            }
+        }
+    });
+
+    $('a[href^="#/"]').click(function() {
+        alert( 'Thank you for clicking, but that\'s a demo link' );
+        return false;
+    })
+});
+
